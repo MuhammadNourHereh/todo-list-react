968237bbbd0cb60ced71aa55b0696054f2831885
Muhammad Nour <acadamiclearner@gmail.com>
2024-11-21 23:53:13 +0200

    handle adding by pressing enter

--- src/components/AddTaskBar.jsx
+++ src/components/AddTaskBar.jsx
@@ -6,19 +6,26 @@ const AddTaskBar = ({ addTask }) => {
 
 
     function addHandle() {
-        if(inputValue == '')
+        setInputValue(inputValue.trim());
+        if (inputValue == '')
             return;
         addTask(inputValue);
         setInputValue('');
         inputRef.current.focus()
     }
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter')
+            addHandle()
 
+    }
     return (
         <div class="input-group mb-3">
             <input type='text' class="form-control"
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 ref={inputRef}
+                autoFocus
+                onKeyDown={handleKeyPress}
             />
             <button class="btn btn-success"
                 onClick={addHandle}>add</button>
